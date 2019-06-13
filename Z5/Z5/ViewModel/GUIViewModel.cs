7671310eaa80b1653aba94ea2655851779a101b4
Patryk Lisik <patrykkbkak@gmail.com>
2019-06-14 01:59:53 +0200

    text bindings does not work

--- Z5/Z5/ViewModel/GUIViewModel.cs
+++ Z5/Z5/ViewModel/GUIViewModel.cs
@@ -1,29 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
 
 namespace ViewModel
 {
-    public class GUIViewModel
+    public class GUIViewModel: INotifyPropertyChanged
     {
         Lazy<WindowPathGeter> WindowPathGeter = new Lazy<WindowPathGeter>();
 
-        public string pathToXMl { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName_)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName_));
+        }
+
+        public string pathToXML;
 
         public ICommand getXML { get; }
+        public string PathToXML
+        {
+            get => pathToXML;
+            set
+            {
+                pathToXML = value;
+                RaisePropertyChanged("PathToXML");
+                Console.WriteLine("notify");
+            }
+            
+        }
 
         public GUIViewModel()
         {
             getXML = new RelayCommand(Browse);
-            Console.WriteLine("dup123");
         }
 
         private void Browse()
         {
-            pathToXMl = "aaaa";
-            Console.WriteLine("clic");
-            pathToXMl = WindowPathGeter.Value.GetPath();
+            PathToXML = WindowPathGeter.Value.GetPath();
+
+            Console.WriteLine(PathToXML);
         }
     }
 }
