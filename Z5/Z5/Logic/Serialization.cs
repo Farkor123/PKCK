225c6c17271015e4442c978138d37f59603fd638
Patryk Lisik <patrykkbkak@gmail.com>
2019-06-15 08:12:42 +0200

    Window layout

--- Z5/Z5/Logic/Serialization.cs
+++ Z5/Z5/Logic/Serialization.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Xml.Serialization;
 
-namespace Data
+namespace Logic
 {
     public class Serialization
     {
--- Z5/Z5/ViewModel/GUIViewModel.cs
+++ Z5/Z5/ViewModel/GUIViewModel.cs
@@ -14,26 +14,66 @@ namespace ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName_));
         }
 
-        public string pathToXML = "aaa";
+        #region DataContex
+        private string pathToXML;
+        private string pathToSchema;
+        private string pathToTransform;
 
         public ICommand getXML { get; }
+        public ICommand getTransform { get; }
+        public ICommand getSchema { get; }
+        public ICommand validate { get; }
+        public ICommand show { get}
+        public ICommand transform { get; }
+
         public string PathToXML
         {
             get => pathToXML;
-            set => pathToXML = value;
+            set
+            {
+                pathToXML = value;
+                RaisePropertyChanged("PathToXML");
+            }
 
         }
 
+        public string PathToSchema
+        {
+            get => pathToSchema;
+            set
+            {
+                pathToSchema = value;
+                RaisePropertyChanged("PathToSchema");
+            }
+        }
+        public string PathToTransform
+        {
+            get => pathToTransform;
+            set
+            {
+                pathToTransform = value;
+                RaisePropertyChanged("PathToTransform");
+            }
+        }
+        #endregion
         public GUIViewModel()
         {
-            getXML = new RelayCommand(Browse);
+            getXML = new RelayCommand(GetXML);
+            getTransform = new RelayCommand(GetTransform);
+            getSchema = new RelayCommand(GetSchema);
         }
 
-        private void Browse()
+        private void GetXML()
         {
-            PathToXML = WindowPathGeter.Value.GetPath();
-            RaisePropertyChanged("PathToXML");
-            Console.WriteLine("notify " + PathToXML);
+            PathToXML = WindowPathGeter.Value.GetPath("xml");
+        }
+        private void GetSchema()
+        {
+            PathToSchema = WindowPathGeter.Value.GetPath("xsd");
+        }
+        private void GetTransform()
+        {
+            PathToTransform = WindowPathGeter.Value.GetPath("xsl");
         }
     }
 }
